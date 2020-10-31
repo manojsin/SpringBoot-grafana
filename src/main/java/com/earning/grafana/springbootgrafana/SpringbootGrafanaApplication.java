985da6081571c50e561bb985eb6fc268cf7e1283
Manoj Prasad Singh <YATRAONLINE\manoj.singh@lap0002517.yatraonline.local>
2020-10-31 21:14:05 +0530

    first commit

---
+++ src/main/java/com/earning/grafana/springbootgrafana/SpringbootGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.earning.grafana.springbootgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/earning/grafana/springbootgrafana/SpringbootGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earning.grafana.springbootgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
